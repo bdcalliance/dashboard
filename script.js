e1e24a391abd2d6bc7f15135b6fec1c578a0ad9b
Juan Lee <juanlee@kaist.ac.kr>
2021-08-27 17:57:00 +0900

    People Edit

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ $(document).ready(() => {
     firebase.initializeApp(firebaseConfig);
 });
 
+/* Alliance */
+
 function load() {
     firebase
         .database()
@@ -170,4 +172,167 @@ function edit(i) {
 function remove(i) {
     window.alliance.splice(i, 1);
     draw();
+}
+
+/* People */
+
+function loadPeople() {
+    firebase
+        .database()
+        .ref("/people")
+        .once("value")
+        .then((snapshot) => {
+            if (snapshot.val()) {
+                let people = snapshot.val();
+                window.people = people;
+                drawPeople();
+            }
+        });
+}
+
+function drawPeople() {
+    $("#people").empty();
+
+    window.people.forEach((person, i) => {
+        $("#people").append(`
+        <div class="ui segment" pid="${i}" >
+            <h4 class="ui header" onclick="clickedPeople(${i})">${person.Name}</h4>
+            <div class="more" style="display: none">
+                <p>Profile: ${person.Profile}</p>
+                <p>Position: ${person.Description}</p>
+                <button class="ui icon button upBtn" onclick="upclickPeople(${i})">
+                Move <i class="angle up icon"></i>
+                </button>
+                <button class="ui icon button downBtn" onclick="downclickPeople(${i})">
+                Move <i class="angle down icon"></i>
+                </button>
+                <button class="ui icon button" onclick="insertupPeople(${i})">
+                Insert <i class="caret square up icon"></i>
+                </button>
+                <button class="ui icon button" onclick="insertdownPeople(${i})">
+                Insert <i class="caret square down icon"></i>
+                </button>
+                <button class="ui icon button" onclick="editPeople(${i})">
+                Edit <i class="edit icon"></i>
+                </button>
+                <button class="ui icon button" onclick="removePeople(${i})">
+                Delete <i class="trash icon"></i>
+                </button>
+            </div>
+            <div id="form" class="ui segment" style="display:none">
+                <div class="field">
+                    <label>Name</label>
+                    <input type="text" name="name${i}" placeholder="Name">
+                </div>
+                <div class="field">
+                    <label>Profile</label>
+                    <input type="text" name="profile${i}" placeholder="Profile">
+                </div>
+                <div class="field">
+                    <label>Position</label>
+                    <input type="text" name="description${i}" placeholder="Position">
+                </div>
+                <button class="ui button">Apply</button>
+            </div>
+        </div>`);
+    });
+    $(`[pid="${0}"] > .more > .upBtn`).addClass("disabled");
+    $(`[pid="${window.alliance.length - 1}"] > .more > .downBtn`).addClass("disabled");
+}
+
+function savePeople() {
+    firebase.database().ref("/people").set(window.people);
+    console.log(window.people);
+    console.log("saved");
+}
+
+function clickedPeople(i) {
+    let elem = $(`div[pid="${i}"] > .more`);
+    if (elem.css("display") === "none") {
+        elem.css("display", "block");
+    } else if (elem.css("display") === "block") {
+        elem.css("display", "none");
+    }
+}
+
+function upclickPeople(i) {
+    if (i === 0) return;
+
+    let temp = window.people[i - 1];
+    window.people[i - 1] = window.people[i];
+    window.people[i] = temp;
+
+    drawPeople();
+}
+
+function downclickPeople(i) {
+    if (i === window.people.length - 1) return;
+
+    let temp = window.people[i + 1];
+    window.people[i + 1] = window.people[i];
+    window.people[i] = temp;
+
+    drawPeople();
+}
+
+function insertupPeople(i) {
+    let elem = $(`div[pid="${i}"] > #form`);
+    if (elem.css("display") === "none") {
+        $(`div[pid="${i}"] > #form > button`).click(() => {
+            window.people.splice(i, 0, {
+                Name: $(`input[name="name${i}"]`).val(),
+                Profile: $(`input[name="profile${i}"]`).val(),
+                Description: $(`input[name="description${i}"]`).val(),
+            });
+
+            drawPeople();
+        });
+        elem.css("display", "block");
+    } else if (elem.css("display") === "block") {
+        elem.css("display", "none");
+    }
+}
+
+function insertdownPeople(i) {
+    let elem = $(`div[pid="${i}"] > #form`);
+    if (elem.css("display") === "none") {
+        $(`div[pid="${i}"] > #form > button`).click(() => {
+            window.people.splice(i + 1, 0, {
+                Name: $(`input[name="name${i}"]`).val(),
+                Profile: $(`input[name="profile${i}"]`).val(),
+                Description: $(`input[name="description${i}"]`).val(),
+            });
+
+            drawPeople();
+        });
+        elem.css("display", "block");
+    } else if (elem.css("display") === "block") {
+        elem.css("display", "none");
+    }
+}
+
+function editPeople(i) {
+    let elem = $(`div[pid="${i}"] > #form`);
+    if (elem.css("display") === "none") {
+        $(`div[pid="${i}"] > #form > button`).click(() => {
+            window.people[i] = {
+                Name: $(`input[name="name${i}"]`).val(),
+                Profile: $(`input[name="profile${i}"]`).val(),
+                Description: $(`input[name="description${i}"]`).val(),
+            };
+
+            drawPeople();
+        });
+        $(`input[name="name${i}"]`).val(window.people[i].Name);
+        $(`input[name="profile${i}"]`).val(window.people[i].Profile);
+        $(`input[name="description${i}"]`).val(window.people[i].Description);
+        elem.css("display", "block");
+    } else if (elem.css("display") === "block") {
+        elem.css("display", "none");
+    }
+}
+
+function removePeople(i) {
+    window.people.splice(i, 1);
+    drawPeople();
 }
